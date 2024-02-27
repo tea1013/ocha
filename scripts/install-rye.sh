37e2fef7874549cf720075291d8acc4792420a4c
tea1013 <tea.enanan@gmail.com>
2024-02-27 21:25:04 +0900

    ...

--- scripts/install-rye.sh
+++ scripts/install-rye.sh
@@ -1,6 +1,5 @@
-#/bin/bash
+#!/bin/bash
 RYE_VERSION=0.27.0
 curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
 source "$HOME/.rye/env"
-export PATH="$HOME/.rye/shims:$PATH"
 rye config --set-bool behavior.use-uv=true
