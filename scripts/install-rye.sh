57ed205317a2950f932bf6852069400e78003610
tea1013 <tea.enanan@gmail.com>
2024-02-27 21:16:31 +0900

    ...

--- scripts/install-rye.sh
+++ scripts/install-rye.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 RYE_VERSION=0.27.0
 curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
-source "$HOME/.rye/env"
+source "$HOME/runner/.rye/env"
 rye config --set-bool behavior.use-uv=true
